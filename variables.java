4018ead92e2b787260faf6f0f9d414e1514d8367
ohba <ohba@sendai-nct.ac.jp>
2024-12-03 14:54:13 +0900

    First commit

---
+++ variables.java
@@ -0,0 +1,31 @@
+public class variables
+{
+	public static void main(String[] args)
+	{
+		boolean bool_data;	// true of flase
+		byte byte_data;		// 1byte integer
+		short short_data;	// 2byte integer
+		int int_data;		// 4byte integer
+		long long_data;		// 8byte integer
+
+		
+		float float_data;	// 4byte floating 
+		double double_data; // 8byte floating
+
+		char moji[]={'h','o','g','e'};	// 1byte integer for character
+
+		bool_data = false;
+		byte_data = 128;
+		short_data = 32767;
+		int_data = 3210;
+		long_data = 1234;
+
+		double_data = 3.14;
+//		byte pi = double_data;
+//		float_data = 3.14F;
+
+		System.out.println(bool_data);
+//		System.out.println("文字列" + "の結合");
+//		System.out.println("文字列" + byte_data + "と数値の結合");
+	}
+}
